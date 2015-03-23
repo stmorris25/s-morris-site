293e155bf5e8056978adae50d0010c81e88a2a7c
Sean Morris <stmorris25@gmail.com>
2015-03-22 22:16:31 -0400

    Lesson 9 - JavaScript

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+function toggle(element) {
+  var e = document.getElementById(element);
+  if(e.style.display == "block") {
+    e.style.display = "none";
+  } else {
+    e.style.display = "block";
+  }
+};
